2ecdd013393174b7a48f505ffccacbc320fe1bdb
konomae <konomae@users.noreply.github.com>
2021-07-18 15:52:44 +0900

    Add initial mock ui

--- PlanningPoker/ContentView.swift
+++ PlanningPoker/ContentView.swift
@@ -1,9 +1,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    var cards: [Int] = [
+        1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+    ]
+    
+    @State var selectedCard: Int?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            Section(header: Text("参加者")) {
+                HStack {
+                    Text("🐶")
+                    
+                    Text("1")
+                }
+            }
+            
+            Section(header: Text("操作")) {
+                HStack {
+                    if selectedCard == nil {
+                        Text("カードを選択してください")
+                    } else {
+                        Text("他のユーザーの投票待ち")
+                    }
+                    
+                    Spacer()
+                    
+                    ProgressView()
+                }
+                
+                Button("リセット", action: reset)
+            }
+            
+            Section(header: Text("カード")) {
+                LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem(), GridItem()]) {
+                    ForEach(cards, id: \.self) { card in
+                        Button(role: .cancel, action: { selectedCard = card }) {
+                            Text(card.description)
+                                .frame(maxWidth: .infinity)
+                        }
+                        .buttonStyle(.bordered)
+                        .tint(selectedCard == card ? .blue : nil)
+                    }
+                }
+            }
+        }
+    }
+    
+    func reset() {
+        selectedCard = nil
     }
 }
 
