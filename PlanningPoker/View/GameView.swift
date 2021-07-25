861ad0f391c8b7beba7dac5c73350f4c35ccfee0
konomae <konomae@users.noreply.github.com>
2021-07-25 10:52:05 +0900

    Add GameView preview

--- PlanningPoker/View/GameView.swift
+++ PlanningPoker/View/GameView.swift
@@ -54,8 +54,62 @@ struct GameView: View {
     }
 }
 
+private extension UUID {
+    static func mock(_ number: Int) -> Self {
+        UUID(uuidString: "00000000-0000-0000-0000-\(String(format: "%012x", number))")!
+    }
+}
+
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        EmptyView()
+        GameView(
+            state: .init(
+                activeParticipants: [.mock(1)],
+                localParticipant: .mock(1),
+                playedCards: []
+            ),
+            playCard: { _ in },
+            reset: {}
+        )
+        
+        GameView(
+            state: .init(
+                activeParticipants: [
+                    .mock(1),
+                    .mock(2),
+                ],
+                localParticipant: .mock(1),
+                playedCards: [
+                    .init(
+                        card: .all[0],
+                        participantID: .mock(1)
+                    ),
+                ]
+            ),
+            playCard: { _ in },
+            reset: {}
+        )
+        
+        GameView(
+            state: .init(
+                activeParticipants: [
+                    .mock(1),
+                    .mock(2),
+                ],
+                localParticipant: .mock(1),
+                playedCards: [
+                    .init(
+                        card: .all[0],
+                        participantID: .mock(1)
+                    ),
+                    .init(
+                        card: .all[1],
+                        participantID: .mock(2)
+                    ),
+                ]
+            ),
+            playCard: { _ in },
+            reset: {}
+        )
     }
 }
